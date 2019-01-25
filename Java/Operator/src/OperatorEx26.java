26edf10cb679319b9fd60af5053d3d8039380703
voldemort <dhyc9395@gmail.com>
2019-01-25 16:13:41 +0900

    Commit 25 january 2019

--- Java/Operator/src/OperatorEx26.java
+++ Java/Operator/src/OperatorEx26.java
@@ -1,4 +1,4 @@
-public class OperatroEx26 {
+public class OperatorEx26 {
     public static void main(String[] args) {
 
         int a = 5;
---
+++ Java/Operator/src/OperatorEx31.java
@@ -0,0 +1,27 @@
+public class OperatorEx31 {
+    public static void main(String[] args) {
+
+
+        int hex = 0xABCD;
+        int mask = 0xF;
+
+        System.out.printf("hex = %X%n", hex);
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+    }
+}
+/*
+hex = ABCD
+D
+C
+B
+A
+ */
