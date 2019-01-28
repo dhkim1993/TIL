71fd9cc8230309ff04466f2ded0a07c09da3fae1
voldemort <dhyc9395@gmail.com>
2019-01-28 19:56:10 +0900

    Commit 28 january 2019

---
+++ Java/if_switch_for_while/src/FlowEx33.java
@@ -0,0 +1,24 @@
+class FlowEx33 {
+    public static void main(String[] args) {
+        Loop1:
+        for (int i = 2; i <= 9; i++) {        //for문에 이름붙임.
+            for (int j = 1; j <= 9; j++) {
+                if (j == 5)
+                    break Loop1;
+//						break;
+//						continue Loop1;
+//						continue;
+                System.out.println(i + "*" + j + "=" + i * j);
+            } // end of for i
+            System.out.println();
+        } // end of Loop1
+
+    }
+}
+/*
+2*1=2
+2*2=4
+2*3=6
+2*4=8
+
+ */
