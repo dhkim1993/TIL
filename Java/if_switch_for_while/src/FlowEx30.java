b0b4e407b60b20a6a4590f643ef7e45a4cdebb73
voldemort <dhyc9395@gmail.com>
2019-01-28 19:18:14 +0900

    Commit 28 january 2019

---
+++ Java/if_switch_for_while/src/FlowEx30.java
@@ -0,0 +1,21 @@
+class FlowEx30 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int i   = 0;
+
+        while(true) {
+            if(sum > 100)
+                break;
+            ++i;
+            sum += i;
+        } // end of while
+
+        System.out.println("i=" + i);
+        System.out.println("sum=" + sum);
+    }
+}
+/*
+i=14
+sum=105
+ */
