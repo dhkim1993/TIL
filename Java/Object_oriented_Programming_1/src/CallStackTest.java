acc46e76baf419bb2795b6a30ad2dbfaf01f3f35
voldemort <dhyc9395@gmail.com>
2019-02-07 14:23:12 +0900

    Commit 7 February 2019

--- Java/Object_oriented_Programming_1/src/CallStackTest.java
+++ Java/Object_oriented_Programming_1/src/CallStackTest.java
@@ -1,6 +1,6 @@
 class CallStackTest {
     public static void main(String[] args) {
-        firstMethod();
+        firstMethod();  //static 메소드는 객체 생성없이 호출이 가능하다.
     }
 
     static void firstMethod() {
