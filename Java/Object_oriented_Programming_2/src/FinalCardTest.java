7bedcc058c7ada672018e034b345ff9c0846701f
voldemort <dhyc9395@gmail.com>
2019-02-09 19:17:56 +0900

    Commit 9 Feb 2019

--- Java/Object_oriented_Programming_2/src/FinalCardTest.java
+++ Java/Object_oriented_Programming_2/src/FinalCardTest.java
@@ -14,7 +14,7 @@ class Card2 {
     static int width = 100;
     static int height = 250;
 
-    Card2(String kind, int num) {
+    Card2(String kind, int num) {   //클래스내에 매개변수를 갖는 생성자를 선언해서 인스턴스 생성할떄 final변수들을 매개변수로 초기화한다.
         KIND = kind;
         NUMBER = num;
     }
