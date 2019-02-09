dab49146b9234a617154519d15baef384ce9bc17
voldemort <dhyc9395@gmail.com>
2019-02-09 18:13:32 +0900

    Commit 9 Feb 2019

---
+++ Java/Object_oriented_Programming_2/src/PackageTest.java
@@ -0,0 +1,11 @@
+package com.javachobo.book;
+
+public class PackageTest {
+    public static void main(String[] args){
+        System.out.println("Hellow World!");
+    }
+}
+/*
+-d 옵션은 소스파일에 지정된 경로를 통해 패키지의 위치를 찾아서 클래스파일을 생성ㄹ한다. 만일 지정된
+패키지와 일치하는 디렉토리가 존재하지 않는다면 자동적으로 생성한다.
+ */
