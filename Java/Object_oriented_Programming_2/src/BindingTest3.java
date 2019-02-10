57c8d353385c6d4d1431fcb533d4baa340570679
voldemort <dhyc9395@gmail.com>
2019-02-10 17:15:32 +0900

    Commit 10 Feb 2019

---
+++ Java/Object_oriented_Programming_2/src/BindingTest3.java
@@ -0,0 +1,43 @@
+public class BindingTest3 {
+    public static void main(String[] args) {
+        Parent2 p = new Child2();
+        Child2 c = new Child2();
+
+        System.out.println("p.x = " + p.x);
+        ((Child2) p).method();
+        System.out.println();
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+}
+
+class Parent2 {
+    int x = 100;
+
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child2 extends Parent2 {
+    int x = 200;
+
+    void method() {
+        System.out.println("x=" + x);
+        System.out.println("super.x=" + super.x);
+        System.out.println("this.x=" + this.x);
+    }
+}
+/*
+p.x = 100
+x=200
+super.x=100
+this.x=200
+
+c.x = 200
+x=200
+super.x=100
+this.x=200
+
+ */
+
