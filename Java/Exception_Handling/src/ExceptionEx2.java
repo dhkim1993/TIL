ea1336c5aac991c828009cb7e7364559f97d05d4
voldemort <dhyc9395@gmail.com>
2019-02-12 17:51:35 +0900

    Commit 12 Feb 2019

---
+++ Java/Exception_Handling/src/ExceptionEx2.java
@@ -0,0 +1,20 @@
+public class ExceptionEx2 {
+    public static void main(String[] args) {
+
+        int number = 100;
+        int result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result = number / (int) (Math.random() * 10);
+            System.out.println(result);
+        }
+    }
+}
+/*
+20
+100
+25
+100
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at ExceptionEx2.main(ExceptionEx2.java:8)
+ */
