2733c9a42c7e97a6497fb3006bf45c555e363fab
voldemort <dhyc9395@gmail.com>
2019-02-13 13:28:34 +0900

    Commit 13 Feb 2019

---
+++ Java/Exception_Handling/src/ExceoptionEx10.java
@@ -0,0 +1,10 @@
+public class ExceoptionEx10 {
+    public static void main(String[] args){
+
+        throw new Exception(); //Exception 을 고의로 발생시킴
+    }
+}
+/*
+예외 처리가 되어야 할 부분에 예외처리가 되어 있지 않다는 에러가 발생.
+예외처리를 해주지 않으면 컴파일 조차 안됨.
+ */
