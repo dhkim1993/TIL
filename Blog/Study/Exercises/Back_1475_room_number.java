0b52a8c552c27f5da78105e903518db26bf2ac3f
voldemort <dhyc9395@gmail.com>
2019-02-28 15:09:49 +0900

    Commit 28 Feb 2019

--- Blog/Study/Exercises/Back_1475_room_number.java
+++ Blog/Study/Exercises/Back_1475_room_number.java
@@ -3,67 +3,39 @@ import java.util.*;
 public class Back_1475_room_number {
     public static void main(String[] args) {
 
-        System.out.print("방번호입력 : ");
-        Scanner input = new Scanner(System.in); //String으로 방숫자를 입력받는다.
-        String rn = input.nextLine();
+        Scanner input = new Scanner(System.in);
+        String n = input.nextLine();
+        String[] arr = n.split("");
+        String[] compareArr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        int[] countArr = new int[10];
 
-        String[] arr = rn.split("");    //입력받은 번호를 각 자리수마다 쪼개서 arr배열에 넣는다.
-
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");         //현재 arr배열을 출력해서 보여준다.
-
-        System.out.println();
-
-        String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};  //String으로 선언해놓고 equals로 비교할꺼다
-        int[] cnt = new int[10];
-
-        for (int i = 0; i < arr.length; i++) {    //입력수만큼
-            for (int j = 0; j < num.length; j++) {    //10
-                if (arr[i].equals(num[j])) {
-                    cnt[j] += 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < compareArr.length; j++) {
+                if (arr[i].equals(Integer.toString(j))) {
+                    countArr[j]++;
                 }
             }
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("%d:%d %n", i, cnt[i]);    //조건에 맞는 숫자가 몇개씩 증가했는지 보여준다.
-        }
 
-        int max = cnt[0];       //제일많이 중복된 값을 추출하기위해 선언하고 첫번째 값을 max에 넣는다.
+        int cnt69;
+        int set;
+        set = countArr[0];
 
-        for (int i = 0; i < num.length; i++) {  //max와 차례차례 비교하여 제일큰값을 max에넣는다.
-            if (max < cnt[i]) {
-                max = cnt[i];
+        for (int i = 1; i < countArr.length; i++) {
+            if (i == 6 || i == 9) {
+                continue;
             }
-            if ((max == cnt[i]) && (max == cnt[6] || max == cnt[9])) {   //6,9와 같은 크기의 숫자가 있으면 그 숫자를 max로 정한다
-                max = cnt[i];
-            }
-        }
-        System.out.printf("max :%d%n", max);
-
-        if (max == cnt[6] || max == cnt[9]) { // 6이나9가 max이면
-            if ((cnt[6] + cnt[9]) % 2 == 0) {     //6+9가 짝수면 /2 하고
-                max = max / 2;
-            } else if ((cnt[6] + cnt[9]) % 2 == 1) {
-                max = max;                      //6+9가 홀수면 그냥 max그대로 ex) 6+9=3이면 2세트가 필요하니까.
+            if (set < countArr[i]) {
+                set = countArr[i];
             }
         }
+        cnt69 = countArr[6] + countArr[9];
+        if (cnt69 % 2 == 1) {
+            cnt69++;
+        }
+        cnt69 = cnt69 / 2;
 
-        System.out.printf("필요한 셋트 갯수는 %d개 입니다.", max);
+        int result = cnt69 > set ? cnt69 : set;
+        System.out.println(result);
     }
 }
-/*
-방번호입력 : 99667329
-9 9 6 6 7 3 2 9
-0:0
-1:0
-2:1
-3:1
-4:0
-5:0
-6:2
-7:1
-8:0
-9:3
-max :3
-필요한 셋트 갯수는 3개 입니다.
- */
