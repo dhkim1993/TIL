64246a6a6d51a165830c5c45864b11b88542af25
voldemort <dhyc9395@gmail.com>
2019-03-12 22:55:43 +0900

    Commit 12 Mar 2019

---
+++ Java/Java.Lang_Package/src/MathEx3.java
@@ -0,0 +1,36 @@
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+class MathEx3 {
+    public static void main(String args[]) {
+        int x1 = 1, y1 = 1;  // (1, 1)
+        int x2 = 2, y2 = 2;  // (2, 2)
+
+        double c = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        double a = c * sin(PI / 4);  // PI/4 rad = 45 degree
+        double b = c * cos(PI / 4);
+//		double b = c * cos(toRadians(45));
+
+        out.printf("a=%f%n", a);
+        out.printf("b=%f%n", b);
+        out.printf("c=%f%n", c);
+        out.printf("angle=%f rad%n", atan2(a, b));
+        out.printf("angle=%f degree%n%n", atan2(a, b) * 180 / PI);
+//		out.printf("angle=%f degree%n%n", toDegrees(atan2(a,b)));
+
+        out.printf("24 * log10(2)=%f%n", 24 * log10(2));  // 7.224720
+        out.printf("53 * log10(2)=%f%n%n", 53 * log10(2));  // 15.954590
+    }
+}
+/*
+a=1.000000
+b=1.000000
+c=1.414214
+angle=0.785398 rad
+angle=45.000000 degree
+
+24 * log10(2)=7.224720
+53 * log10(2)=15.954590
+
+
+ */
