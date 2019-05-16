50dde5e7c4e3ad981d006456eb19c50851dd48b8
voldemort <dhyc9395@gmail.com>
2019-05-16 19:45:50 +0900

    Commit 16 May 2019

---
+++ Java/InputOutput/src/InputOutput_Package/IOEx3.java
@@ -0,0 +1,50 @@
+package InputOutput_Package;
+
+import java.io.*;
+import java.util.Arrays;
+
+class IOEx3 {
+    public static void main(String[] args) {
+        byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+        byte[] outSrc = null;
+
+        byte[] temp = new byte[4];	// 이전 예제와 배열의 크기가 다르다.
+
+        ByteArrayInputStream input = null;
+        ByteArrayOutputStream output = null;
+
+        input  = new ByteArrayInputStream(inSrc);
+        output = new ByteArrayOutputStream();
+
+        System.out.println("Input Source  :" + Arrays.toString(inSrc));
+
+        try {
+            while(input.available() > 0) {
+                input.read(temp);
+                output.write(temp);
+//				System.out.println("temp :" + Arrays.toString(temp));
+
+                outSrc = output.toByteArray();
+                printArrays(temp, outSrc);
+            }
+        } catch(IOException e) {}
+    } // main의 끝
+
+    static void printArrays(byte[] temp, byte[] outSrc) {
+        System.out.println("temp          :" +Arrays.toString(temp));
+        System.out.println("Output Source :" +Arrays.toString(outSrc));
+    }
+}
+
+/*
+Input Source  :[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+temp          :[0, 1, 2, 3]
+Output Source :[0, 1, 2, 3]
+temp          :[4, 5, 6, 7]
+Output Source :[0, 1, 2, 3, 4, 5, 6, 7]
+temp          :[8, 9, 6, 7]
+Output Source :[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 7]
+
+Process finished with exit code 0
+
+ */
