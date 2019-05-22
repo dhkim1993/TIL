a3d65f4dcd2d87aa12f04532617f80056ee6b9cb
voldemort <dhyc9395@gmail.com>
2019-05-22 20:09:17 +0900

    Commit 22 May 2019

---
+++ Java/Network/src/Network_Package/NetworkEx1.java
@@ -0,0 +1,70 @@
+package Network_Package;
+
+import java.net.*;
+import java.util.*;
+
+class NetworkEx1 {
+    public static void main(String[] args)
+    {
+        InetAddress ip = null;
+        InetAddress[] ipArr = null;
+
+        try {
+            ip = InetAddress.getByName("www.naver.com");
+            System.out.println("getHostName() :"   +ip.getHostName());
+            System.out.println("getHostAddress() :"+ip.getHostAddress());
+            System.out.println("toString() :"      +ip.toString());
+
+            byte[] ipAddr = ip.getAddress();
+            System.out.println("getAddress() :"+Arrays.toString(ipAddr));
+
+            String result = "";
+            for(int i=0; i < ipAddr.length;i++) {
+                result += (ipAddr[i] < 0) ? ipAddr[i] + 256 : ipAddr[i];
+                result += ".";
+            }
+            System.out.println("getAddress()+256 :"+result);
+            System.out.println();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ip = InetAddress.getLocalHost();
+            System.out.println("getHostName() :"   +ip.getHostName());
+            System.out.println("getHostAddress() :"+ip.getHostAddress());
+            System.out.println();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            ipArr = InetAddress.getAllByName("www.naver.com");
+
+            for(int i=0; i < ipArr.length; i++) {
+                System.out.println("ipArr["+i+"] :" + ipArr[i]);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    } // main
+}
+/*
+/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java "-javaagent:/Applications/IntelliJ IDEA.app/Contents/lib/idea_rt.jar=53388:/Applications/IntelliJ IDEA.app/Contents/bin" -Dfile.encoding=UTF-8 -classpath /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/charsets.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/deploy.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/cldrdata.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/dnsns.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/jaccess.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/jfxrt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/localedata.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/nashorn.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/sunec.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/sunjce_provider.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/sunpkcs11.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/ext/zipfs.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/javaws.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/jce.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/jfr.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/jfxswt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/jsse.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/management-agent.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/plugin.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/resources.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/ant-javafx.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/dt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/javafx-mx.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/jconsole.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/packager.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/sa-jdi.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/lib/tools.jar:/Users/voldemort/IdeaProjects/TIL/out Network_Package.NetworkEx1
+getHostName() :www.naver.com
+getHostAddress() :125.209.222.141
+toString() :www.naver.com/125.209.222.141
+getAddress() :[125, -47, -34, -115]
+getAddress()+256 :125.209.222.141.
+
+getHostName() :Tripui-MacBookPro.local
+getHostAddress() :127.0.0.1
+
+ipArr[0] :www.naver.com/125.209.222.141
+ipArr[1] :www.naver.com/125.209.222.142
+
+Process finished with exit code 0
+
+ */
