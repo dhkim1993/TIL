54228e6b562ce0046f95e5c00cf73390d1433863
dhkim1993 <dhyc9395@gmail.com>
2019-09-25 14:48:58 +0900

    TIL-2019.09.25 Wed Commit

--- CodingTest/Programmers/CustomString.java
+++ CodingTest/Programmers/CustomString.java
@@ -7,20 +7,17 @@ import java.util.Comparator;
 public class CustomString {
     public static void main(String[] args) {
         String[] strings = {"tcv", "yyye", "tcv", "qaz"};
-        int n = 1;
+        int n = 2;
         CustomString customString = new CustomString();
         String[] result = customString.solution(strings, n);
         for(String s : result) System.out.print(s+" ");
     }
     public String[] solution(String[] strings, int n){
-        Arrays.sort(strings, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if(s1.charAt(n) > s2.charAt(n)) return 1;
-                else if(s1.charAt(n) < s2.charAt(n)) return s1.compareTo(s2);
-                else if(s1.charAt(n) == s2.charAt(n)) return 0;
-                else return 0;
-            }
+        Arrays.sort(strings, (s1, s2) -> {
+            if(s1.charAt(n) > s2.charAt(n)) return 1;
+            else if(s1.charAt(n) < s2.charAt(n)) return 0;
+            else if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+            else return 0;
         });
         return strings;
     }
